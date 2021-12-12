22889b7491d08219b36f3e8883497a5869a8d643
Tarek Massini <tarek.jannis.03.tm@gmail.com>
2021-12-12 22:16:27 +0100

    enable static export

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  productionBrowserSourceMaps: true,
   reactStrictMode: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      '/': { page: '/' },
+      '/browse': {page: '/browse'}
+    }
+  },
 }
